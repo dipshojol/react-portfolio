6535b1b792c53376baae370b2e7d6b572e46a05b
shojol <dip_shojol@yahoo.com>
2019-07-23 22:30:42 -0400

    Updates

---
+++ precache-manifest.2d086da5d34ca8672ab69224eb2097cc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c391357af28130b8e264692b22932e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "87c9cbc65e19a889c0ce",
+    "url": "/react-portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "503b97b0d66a8073dc41",
+    "url": "/react-portfolio/static/css/main.a14e5c0d.chunk.css"
+  },
+  {
+    "revision": "87c9cbc65e19a889c0ce",
+    "url": "/react-portfolio/static/js/2.34ad463a.chunk.js"
+  },
+  {
+    "revision": "503b97b0d66a8073dc41",
+    "url": "/react-portfolio/static/js/main.1eed6de7.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "770df772afbafba7f0d15d100a80ab35",
+    "url": "/react-portfolio/static/media/3.770df772.jpg"
+  },
+  {
+    "revision": "605b20b52b3a9756dd18b7ae4d7b2845",
+    "url": "/react-portfolio/static/media/background01.605b20b5.webm"
+  },
+  {
+    "revision": "93ba41f206fd0eeece1756d50599d358",
+    "url": "/react-portfolio/static/media/discussionsApple.93ba41f2.png"
+  },
+  {
+    "revision": "82bbaf73ebc77a33586bf0d110da6b07",
+    "url": "/react-portfolio/static/media/movieDB.82bbaf73.png"
+  },
+  {
+    "revision": "8bc271249cf40381274b23a59ecc3616",
+    "url": "/react-portfolio/static/media/react-color-palette.8bc27124.png"
+  },
+  {
+    "revision": "b77be0444639ca58f19bc3c1f6dd5c34",
+    "url": "/react-portfolio/static/media/react-portfolio.b77be044.png"
+  },
+  {
+    "revision": "68e25da8016d63882cc5b00632642c2d",
+    "url": "/react-portfolio/static/media/triabeauty.68e25da8.png"
+  }
+]);
