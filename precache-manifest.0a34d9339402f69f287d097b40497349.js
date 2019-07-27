8a253ac85cee0c29a247409dea840f4de17c4777
shojol <dip_shojol@yahoo.com>
2019-07-27 15:09:01 -0400

    Updates

--- precache-manifest.0a34d9339402f69f287d097b40497349.js
+++ precache-manifest.0a34d9339402f69f287d097b40497349.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3b0561178c9d8bd92268c6cc66be7273",
+    "revision": "83c7709fef03204e3042aa74e2fd65be",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "287d07dd6e3324fe07e1",
+    "revision": "c428bd26d290f82feecc",
     "url": "/react-portfolio/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "48ca6ed86266c62fcdf7",
-    "url": "/react-portfolio/static/css/main.9c36f59d.chunk.css"
+    "revision": "206356f1210609779d03",
+    "url": "/react-portfolio/static/css/main.e6d69657.chunk.css"
   },
   {
-    "revision": "287d07dd6e3324fe07e1",
-    "url": "/react-portfolio/static/js/2.4c3e9549.chunk.js"
+    "revision": "c428bd26d290f82feecc",
+    "url": "/react-portfolio/static/js/2.7faa946f.chunk.js"
   },
   {
-    "revision": "48ca6ed86266c62fcdf7",
-    "url": "/react-portfolio/static/js/main.8befec31.chunk.js"
+    "revision": "206356f1210609779d03",
+    "url": "/react-portfolio/static/js/main.8fed7a2d.chunk.js"
   },
   {
     "revision": "94872cf7e81768d148a3",
@@ -36,12 +36,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/media/discussionsApple.93ba41f2.png"
   },
   {
+    "revision": "b805a11f2cff00ff99feff89f4546644",
+    "url": "/react-portfolio/static/media/email.b805a11f.svg"
+  },
+  {
     "revision": "c548fcb93e5ea0f147fdd7705a9275a8",
     "url": "/react-portfolio/static/media/express.c548fcb9.png"
   },
   {
     "revision": "18fa4bfb6bcde6795f500385dcf1e231",
     "url": "/react-portfolio/static/media/jive.18fa4bfb.png"
+  },
+  {
+    "revision": "b971fb1410c3a846ef4f97e14e6d5432",
+    "url": "/react-portfolio/static/media/linkedin.b971fb14.svg"
   },
   {
     "revision": "82bbaf73ebc77a33586bf0d110da6b07",
@@ -54,6 +62,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b77be0444639ca58f19bc3c1f6dd5c34",
     "url": "/react-portfolio/static/media/react-portfolio.b77be044.png"
+  },
+  {
+    "revision": "b1f03de45a9607d85e06343d545b900e",
+    "url": "/react-portfolio/static/media/shojolsResume.b1f03de4.pdf"
+  },
+  {
+    "revision": "8f630bc789860f4a168bdc428e2d45e7",
+    "url": "/react-portfolio/static/media/telephone.8f630bc7.svg"
   },
   {
     "revision": "68e25da8016d63882cc5b00632642c2d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.a2b0f2af08c9e1a82cf00de9a596ddb8.js"
+  "/react-portfolio/precache-manifest.0a34d9339402f69f287d097b40497349.js"
 );
 
 self.addEventListener('message', (event) => {
